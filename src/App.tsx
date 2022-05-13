5932f828a13c71b3a7b8d959e055b0c8e003bad4
Dave Iverson <daveiverson@thoughtbot.com>
2022-05-13 13:50:14 -0500

    Added an "about" blurb and link to Github

--- src/App.tsx
+++ src/App.tsx
@@ -103,16 +103,23 @@ function App() {
   return (
     <div className="flex flex-col sm:flex-row">
       <div className="p-4 w-[30%] min-w-[400px]">
-        <h1 className="mb-1 text-4xl font-bold">Dither Machine</h1>
-        <p className="mb-8">
-          A port of{' '}
-          <a
-            href="https://lunarlabs.itch.io/dither-machine"
-            className="underline font-bold"
-          >
-            Dither Machine by Lunar Labs
-          </a>
-        </p>
+        <header>
+          <h1 className="mb-1 text-4xl font-bold">Dither Machine</h1>
+          <p className="mb-2">
+            A port of{' '}
+            <a
+              href="https://lunarlabs.itch.io/dither-machine"
+              className="underline font-bold"
+            >
+              Dither Machine by Lunar Labs
+            </a>
+          </p>
+          <p className="mb-8">
+            Dither Machine is a tool to help you generate dithering
+            automatically, instead of doing it by hand, which can be a tedious
+            task.
+          </p>
+        </header>
         <Controls
           setAngle={setAngle}
           angle={angle}
@@ -138,6 +145,22 @@ function App() {
           radialGradientScale={radialGradientScale}
           setRadialGradientScale={setRadialGradientScale}
         />
+        <footer className="mt-12">
+          <a
+            href="https://github.com/pangolingo/dither-machine-web"
+            className="block underline font-bold"
+          >
+            <svg
+              viewBox="0 0 16 16"
+              className="w-5 h-5 inline-block"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+            </svg>
+            <span className="align-middle ml-2">Check it out on Github</span>
+          </a>
+        </footer>
       </div>
       <div className="p-4">
         <Canvas draw={draw} canvasSize={canvasSize} />
